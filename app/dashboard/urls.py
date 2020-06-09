d5c31a1f6a735b93c2c3c87e56d56a3cc8850203
wutianshu <wutianshu@daojia-inc.com>
2020-06-09 23:38:40 +0800

    设置初始化文件

--- app/dashboard/urls.py
+++ app/dashboard/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from app.dashboard.views.base import Base
+from app.dashboard.views.base import Index, Admin
+from app.dashboard.views.extends import Extends
 
 urlpatterns = [
-    path('base', Base.as_view(), name='base'),
+    path('', Index.as_view(), name='index'),
+    path('admin', Admin.as_view(), name='admin'),
 
 ]
--- app/dashboard/views/base.py
+++ app/dashboard/views/base.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from django.views.generic import View
 
 
-class Base(View):
-    TEMPLATE = 'dashboard/base.html'
+class Index(View):
+    TEMPLATE = 'dashboard/index.html'
+
+    def get(self, request):
+        return render(request, self.TEMPLATE)
+
+
+class Admin(View):
+    TEMPLATE = 'dashboard/admin.html'
 
     def get(self, request):
         return render(request, self.TEMPLATE)
