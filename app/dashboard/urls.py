22fe97fa42e0cf489fdf313e7c84e75f43f97d88
wutianshu <wutianshu@daojia-inc.com>
2020-06-11 22:19:21 +0800

    完成dashboard模块

--- app/dashboard/urls.py
+++ app/dashboard/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from app.dashboard.views.base import Index, Admin, User_login, Register, User_logout
+from app.dashboard.views.base import Index, Admin, User_login, Register, User_logout, Admin_setting
 from app.dashboard.views.extends import Extends
 
 urlpatterns = [
@@ -8,5 +8,5 @@ urlpatterns = [
     path('login', User_login.as_view(), name='login'),
     path('register', Register.as_view(), name='register'),
     path('logout', User_logout.as_view(), name='logout'),
-
+    path('admin/setting', Admin_setting.as_view(), name='admin_setting'),
 ]
--- app/dashboard/views/base.py
+++ app/dashboard/views/base.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect, reverse
 from django.views.generic import View
 from django.contrib.auth.models import User
-from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.hashers import make_password
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 
 class Index(View):
@@ -19,10 +20,28 @@ class Index(View):
 
 class Admin(View):
     TEMPLATE = 'dashboard/admin.html'
+    TEMPLATE_PERMISSION = 'dashboard/auth/permission.html'
 
     @method_decorator(login_required(login_url='login'))
     def get(self, request):
-        return render(request, self.TEMPLATE)
+        if request.user.is_superuser:
+            username = request.GET.get("username", "")
+            page = request.GET.get("page", 1)
+            page = int(page)
+            if username:
+                users = User.objects.filter(username__contains=username).order_by('id')
+            else:
+                users = User.objects.all().order_by('id')
+            total_users = users.count()
+            p = Paginator(users, 10)
+            current_page = p.get_page(page)
+            users = current_page.object_list
+            total_page = p.num_pages
+            return render(request, self.TEMPLATE,
+                          {"users": users, "total_page": total_page, "total_users": total_users, "current_page": page})
+        else:
+            return render(request, self.TEMPLATE_PERMISSION)
+
 
 class User_login(View):
     TEMPLATE = 'dashboard/auth/login.html'
@@ -44,12 +63,14 @@ class User_login(View):
         login(request, user)
         return redirect(reverse("index"))
 
+
 class User_logout(View):
     TEMPLATE = 'dashboard/auth/login.html'
 
     def get(self, request):
         logout(request)
         return render(request, self.TEMPLATE)
+
 
 class Register(View):
     TEMPLATE = 'dashboard/auth/register.html'
@@ -61,7 +82,6 @@ class Register(View):
         username = request.POST.get("username")
         password = request.POST.get("password")
         chk_pwd = request.POST.get("check_password")
-        print(username,password,chk_pwd)
         if not username:
             data = {'error': '用户名不能为空'}
             return render(request, self.TEMPLATE, data)
@@ -78,3 +98,16 @@ class Register(View):
 
         User.objects.create(username=username, password=make_password(password))
         return redirect(reverse('login'))
+
+
+class Admin_setting(View):
+    def get(self, request):
+        seeting = request.GET.get("setting")
+        username = request.GET.get("username")
+        user = User.objects.get(username=username)
+        if seeting == 'on':
+            user.is_superuser = True
+        elif seeting == 'off':
+            user.is_superuser = False
+        user.save()
+        return redirect(reverse('admin'))
