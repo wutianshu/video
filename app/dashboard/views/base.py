5258a4f9df5857de0b09277b39a49e297dd00c6e
wutianshu <wutianshu@daojia-inc.com>
2020-06-12 11:07:43 +0800

    完成dashboard模块

--- app/dashboard/views/base.py
+++ app/dashboard/views/base.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.views.generic import View
 from django.contrib.auth.models import User
-from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.hashers import make_password
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
@@ -41,63 +39,6 @@ class Admin(View):
                           {"users": users, "total_page": total_page, "total_users": total_users, "current_page": page})
         else:
             return render(request, self.TEMPLATE_PERMISSION)
-
-
-class User_login(View):
-    TEMPLATE = 'dashboard/auth/login.html'
-
-    def get(self, request):
-        return render(request, self.TEMPLATE)
-
-    def post(self, request):
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        print(username, password)
-        if not User.objects.filter(username=username).exists():
-            data = {"error": "用户名不存在"}
-            return render(request, self.TEMPLATE, data)
-        user = authenticate(username=username, password=password)
-        if not user:
-            data = {"error": "密码错误"}
-            return render(request, self.TEMPLATE, data)
-        login(request, user)
-        return redirect(reverse("index"))
-
-
-class User_logout(View):
-    TEMPLATE = 'dashboard/auth/login.html'
-
-    def get(self, request):
-        logout(request)
-        return render(request, self.TEMPLATE)
-
-
-class Register(View):
-    TEMPLATE = 'dashboard/auth/register.html'
-
-    def get(self, request):
-        return render(request, self.TEMPLATE)
-
-    def post(self, request):
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        chk_pwd = request.POST.get("check_password")
-        if not username:
-            data = {'error': '用户名不能为空'}
-            return render(request, self.TEMPLATE, data)
-        if not password:
-            data = {'error': '密码不能为空'}
-            return render(request, self.TEMPLATE, data)
-        if password != chk_pwd:
-            data = {'error': '两次密码不一致'}
-            return render(request, self.TEMPLATE, data)
-        user = User.objects.filter(username=username).exists()
-        if user:
-            data = {'error': '用户名已存在'}
-            return render(request, self.TEMPLATE, data)
-
-        User.objects.create(username=username, password=make_password(password))
-        return redirect(reverse('login'))
 
 
 class Admin_setting(View):
